d14d428f73cc1cb344c9ecbdb8cc13276a3a4d22
Punam Dhomse <punam.dhomsee@aressindia.net>
2019-09-06 10:36:42 +0530

    Assignment4,5

---
+++ Assignment4,5/MVCUsingAspDotnetCore/MVCUsingAspDotnetCore/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MVCUsingAspDotnetCore.Models;
+
+namespace MVCUsingAspDotnetCore.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        #region "Viewmodel"
+        public string Title { get; set; }
+        public Employee Employee { get; set; }
+        #endregion
+    }
+}
